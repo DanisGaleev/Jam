4a95269e4fd54d1e35cf3d3254507b04052c41b3
Deniz Mullagaliev <72813969+qxdeniz@users.noreply.github.com>
2024-12-07 23:05:42 +0300

    Update thema.gd

--- thema/thema.gd
+++ thema/thema.gd
@@ -4,12 +4,40 @@ extends Panel
 @onready var date = $MarginContainer/VBoxContainer/HBoxContainer2/Date
 @onready var title = $MarginContainer/VBoxContainer/Title
 @onready var body = $MarginContainer/VBoxContainer/Body
-@onready var image: TextureRect = $MarginContainer/VBoxContainer/Image
 @onready var views = $MarginContainer/VBoxContainer/HBoxContainer/Views
 
+@onready var comments_container = $MarginContainer/VBoxContainer/commetns_container
+@onready var comment_input = $MarginContainer/VBoxContainer/comment_input
+@onready var send_button = $MarginContainer/VBoxContainer/send_button
+
+
 var comments: Array = []
+
+
 
 func _on_comments_pressed():
 	Main.theme_title = title.text
 	Main.theme_body = body.text
 	get_tree().change_scene_to_file("res://thema/theme_view.tscn")
+
+func _on_send_comment():
+	var comment_text = comment_input.text.strip_edges()
+	if comment_text == "":
+		print("Комментарий не может быть пустым!")
+		return
+	
+
+	var comment = {
+		"user_name": "user_name",  
+		"comment_date": "23:56",
+		"text": comment_text
+	}
+	comments.append(comment)
+	
+
+	var comment_label = Label.new()
+	comment_label.text = comment.user_name + ": " + comment.text
+	comments_container.add_child(comment_label)
+
+	comment_input.text = ""
+	print("Комментарий добавлен!")

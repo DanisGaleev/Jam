aa2cad2dc2bfea5bb6ca886f113d01a5fce55fa0
qxdeniz <mullagalievdeniz@yandex.ru>
2024-12-07 23:56:26 +0300

    comments upload

--- main/main.gd
+++ main/main.gd
@@ -22,4 +22,38 @@ func _ready():
 			pst.image.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT
 			pst.image.size_flags_stretch_ratio = 8
 			pst.custom_minimum_size.y = 648
-		pst.views.text = str(post.views)
+		#pst.views.text = str(post.views)
+		for comment in post.comments:
+			var comment_label = Label.new()
+			comment_label.text = comment.user_name + ": " + comment.text
+			pst.comments_container.add_child(comment_label)
+		
+		
+		pst.send_button.connect(
+			"pressed",
+			Callable(self, "_on_send_comment").bind(post, pst),
+			3)
+
+
+func _on_send_comment(post: Dictionary, post_node: Node):
+	var input_field = post_node.comment_input
+	var comment_text = input_field.text.strip_edges()
+	
+	if comment_text == "":
+		print("Комментарий не может быть пустым!")
+		return
+	
+	var new_comment = {
+		"user_name": "USER", 
+		"comment_date": "12.00", 
+		"text": comment_text
+	}
+
+	post.comments.append(new_comment)
+	
+	var comment_label = Label.new()
+	comment_label.text = new_comment.user_name + ": " + new_comment.text
+	post_node.comments_container.add_child(comment_label)
+	
+	input_field.text = ""
+	print("Комментарий добавлен!")
--- thema/thema.gd
+++ thema/thema.gd
@@ -7,9 +7,35 @@ extends Panel
 @onready var image: TextureRect = $MarginContainer/VBoxContainer/Image
 @onready var views = $MarginContainer/VBoxContainer/HBoxContainer/Views
 
+@onready var comments_container = $MarginContainer/VBoxContainer/comments_container
+@onready var comment_input = $MarginContainer/VBoxContainer/comment_input
+@onready var send_button = $MarginContainer/VBoxContainer/send_button
+
 var comments: Array = []
+
+
 
 func _on_comments_pressed():
 	Main.theme_title = title.text
 	Main.theme_body = body.text
 	get_tree().change_scene_to_file("res://thema/theme_view.tscn")
+
+func _on_send_comment():
+	var comment_text = comment_input.text.strip_edges()
+	if comment_text == "":
+		print("Комментарий не может быть пустым!")
+		return
+
+	var comment = {
+		"user_name": "USER",  
+		"comment_date": "12:22",
+		"text": comment_text
+	}
+	comments.append(comment)
+
+	var comment_label = Label.new()
+	comment_label.text = comment.user_name + ": " + comment.text
+	comments_container.add_child(comment_label)
+	
+	comment_input.text = ""
+	print("Комментарий добавлен!")
